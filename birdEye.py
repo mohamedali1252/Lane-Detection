00e3e26027a552789d8f455c8a302a49bf2057af
Ahmadniazy <47083667+Ahmadniazy@users.noreply.github.com>
2022-04-19 16:41:07 +0200

    Add files via upload

---
+++ birdEye.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+
+def perspective_transform():
+    src = np.float32([(550, 460),  # top-left
+                      (150, 720),  # bottom-left
+                      (1200, 720),  # bottom-right
+                      (770, 460)])  # top-right
+    dst = np.float32([(100, 0),
+                      (100, 720),
+                      (1100, 720),
+                      (1100, 0)])
+    M = cv2.getPerspectiveTransform(src, dst)
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    return M, Minv
+
+
+def warp_perspective(img, imgsize, M):
+    return cv2.warpPerspective(img, M, imgsize, cv2.INTER_LINEAR)
