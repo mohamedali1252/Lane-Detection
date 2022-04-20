5a9aafa7a0f6fc5337a1f6fb660c668014a2c81b
Ahmadniazy <47083667+Ahmadniazy@users.noreply.github.com>
2022-04-20 18:25:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import sys
 from moviepy.editor import VideoFileClip
 from ProcessImage import ProcessImage
 
-# input_video = sys.argv[1]
-# output_video = sys.argv[2]
-# debugMode = False if sys.argv[3] == 0 else True
-# print(input_video, output_video, debugMode)
 
-process = ProcessImage(debugMode = True)
+input_video = sys.argv[1]
+output_video = sys.argv[2]
+debugMode = int(sys.argv[3])
+print(debugMode)
+print(input_video, output_video, debugMode)
 
-input_video = '../challenge_video.mp4'
-output_video = 'output_video.mp4'
+
+# input_video = "challenge_video.mp4"
+# output_video = "out.mp4"
+# debugMode = True
+
+process = ProcessImage(debugMode = debugMode)
+
+
+#input_video = '../challenge_video.mp4'
+#output_video = 'output_video.mp4'
 
 clip = VideoFileClip(input_video)
 
