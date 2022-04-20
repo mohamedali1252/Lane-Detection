d91662d7abd341635e8489ca0dbf5c6779919b95
mo1998 <mo.181298@gmail.com>
2022-04-20 14:51:36 +0200

    Fixed LaneLines.py

--- LaneLines.py
+++ LaneLines.py
@@ -68,5 +68,102 @@ class LaneLines:
 
         return leftx, lefty, rightx, righty, out_img
 
+    def fit_poly(self, img):
+        leftx, lefty, rightx, righty, out_img = self.find_lane_pixels(img)
+
+        if len(lefty) > 1500:
+            self.left_fit = np.polyfit(lefty, leftx, 2)
+        if len(righty) > 1500:
+            self.right_fit = np.polyfit(righty, rightx, 2)
+
+        maxy = img.shape[0] - 1
+        miny = img.shape[0] // 3
+        if len(lefty):
+            miny = min(miny, np.min(lefty))
+
+        if len(righty):
+            miny = min(miny, np.min(righty))
+
+        ploty = np.linspace(miny, maxy, img.shape[0])
+        left_fitx = self.left_fit[0] * ploty ** 2 + self.left_fit[1] * ploty + self.left_fit[2]
+        right_fitx = self.right_fit[0] * ploty ** 2 + self.right_fit[1] * ploty + self.right_fit[2]
+        out_img1= np.copy(out_img)
+        for i, y in enumerate(ploty):
+            l = int(left_fitx[i])
+            r = int(right_fitx[i])
+            y = int(y)
+            cv2.line(out_img, (l, y), (r, y), (0, 255, 0))
+            cv2.line(out_img, (r, y), (r, y), (255, 0, 0),10)
+            cv2.line(out_img, (l, y), (l, y), (255, 0, 0),10)
+            cv2.line(out_img1, (r, y), (r, y), (255, 0, 0),5)
+            cv2.line(out_img1, (l, y), (l, y), (255, 0, 0),5)
+
+        lR, rR, pos = self.measure_curvature()
+
+        return out_img, out_img1
+
+    def plot(self, out_img):
+        np.set_printoptions(precision=6, suppress=True)
+        lR, rR, pos = self.measure_curvature()
+
+        value = None
+        if abs(self.left_fit[0]) > abs(self.right_fit[0]):
+            value = self.left_fit[0]
+        else:
+            value = self.right_fit[0]
+
+        if abs(value) <= 0.00015:
+            self.dir.append('F')
+        elif value < 0:
+            self.dir.append('L')
+        else:
+            self.dir.append('R')
+
+        if len(self.dir) > 10:
+            self.dir.pop(0)
+
+        W = 400
+        H = 300
+        widget = np.copy(out_img[:H, :W])
+        widget //= 2
+        widget[0, :] = [0, 0, 255]
+        widget[-1, :] = [0, 0, 255]
+        widget[:, 0] = [0, 0, 255]
+        widget[:, -1] = [0, 0, 255]
+        out_img[:H, :W] = widget
+
+        direction = max(set(self.dir), key=self.dir.count)
+        curvature_msg = "Curvature = {:.0f} m".format(min(lR, rR))
+
+        if direction in 'LR':
+            cv2.putText(out_img, curvature_msg, org=(10, 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                        color=(255, 255, 255), thickness=2)
+
+        cv2.putText(
+            out_img,
+            "Vehicle is {:.2f} m away from center".format(pos),
+            org=(10, 200),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.66,
+            color=(255, 255, 255),
+            thickness=2)
+
+        return out_img
+
+    def measure_curvature(self):
+        ym = 30 / 720
+        xm = 3.7 / 700
+
+        left_fit = self.left_fit.copy()
+        right_fit = self.right_fit.copy()
+        y_eval = 700 * ym
+        left_curveR = ((1 + (2 * left_fit[0] * y_eval + left_fit[1]) ** 2) ** 1.5) / np.absolute(2 * left_fit[0])
+        right_curveR = ((1 + (2 * right_fit[0] * y_eval + right_fit[1]) ** 2) ** 1.5) / np.absolute(2 * right_fit[0])
+
+        xl = np.dot(self.left_fit, [700 ** 2, 700, 1])
+        xr = np.dot(self.right_fit, [700 ** 2, 700, 1])
+        pos = (1280 // 2 - (xl + xr) // 2) * xm
+        return left_curveR, right_curveR, pos
+
     
 
